a92f48098f5d4465423f59776cdaf48e429eeda4
DanielRoa <danbron523@gmail.com>
2019-11-27 18:51:15 -0500

    añadi la funcionalidad de cambiar el color en el layout de configuracion

--- app/src/main/java/mi/aplicacion/PromediaTuSemestre/ActivityConfiguracion.java
+++ app/src/main/java/mi/aplicacion/PromediaTuSemestre/ActivityConfiguracion.java
@@ -1,19 +1,48 @@
 package mi.aplicacion.PromediaTuSemestre;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class ActivityConfiguracion extends AppCompatActivity {
+
+    private Switch fondo;
+    private ConstraintLayout cajon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_configuracion);
 
+        fondo = (Switch) findViewById(R.id.fondo);
+        cajon = (ConstraintLayout) findViewById(R.id.cajon);
+
+        Resources resources = getResources();
+
+        fondo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                    cajon.setBackgroundColor(Color.BLACK);
+
+                } else {
+
+                    cajon.setBackgroundColor(Color.WHITE);
+
+                }
+
+            }
+
+        });
+
     }
 
 }
+
+
